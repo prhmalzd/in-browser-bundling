97b5c5ded9834c9c66a1ee9a0e8e441fd29dfb7c
parham <prhmalzd@gmail.com>
2023-10-21 12:49:25 +0330

    some update for bundler plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 import {useState , useEffect, useRef} from 'react'
 import * as esbuild from 'esbuild-wasm'
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
   const ref = useRef<any>()
@@ -23,12 +24,14 @@ const App = () => {
       return
     }
 
-    const result = await ref.current.transform(input, {
-      loader: 'jsx',
-      target: 'es2015'
-    });
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
+    })
 
-    setCode(result.code)
+    setCode(result.outputFiles[0].text)
   }
 
   return <div>
